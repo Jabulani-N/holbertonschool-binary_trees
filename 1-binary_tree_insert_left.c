555f375402e45c66fee7784af543921c48fbd966
Jabulani-N <jabu.94@gmail.com>
2023-04-24 15:27:54 -0700

    returns_null_if_parent_null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,8 +15,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = binary_tree_node(parent, value);
+	binary_tree_t *newNode;
 	/*node created*/
+
+	if (!parent)/*quit if parent is null*/
+		return(NULL);
+	newNode = binary_tree_node(parent, value);
 
 	if (!(newNode)) /*if malloc within node creation failed*/
 		return (NULL);
