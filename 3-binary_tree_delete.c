9e953ee23143ec3de695fea4a979edf4a081d9a0
Jabulani-N <jabu.94@gmail.com>
2023-04-24 17:50:00 -0700

    making_sure_all_unified

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_delete - deletes binary tree
+ * @parent : pointer
+ * Return: jk
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+}
+
